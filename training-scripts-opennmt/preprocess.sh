cbb2371c3927ce1906ddb209d24097e25902268b
Antoni Oliver <aoliverg@uoc.edu>
2021-01-25 09:46:29 +0100

    new training scripts opennmt

---
+++ training-scripts-opennmt/preprocess.sh
@@ -0,0 +1,1 @@
+onmt_preprocess -train_src train.sp.en -train_tgt train.sp.es -valid_src val.sp.en -valid_tgt val.sp.es -train_align train.sp.en.es.align -valid_align val.sp.en.es.align -save_data data
